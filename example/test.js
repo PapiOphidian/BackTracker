b2d1621a311a687e0cc767aafaf01334da521ed0
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-05-16 20:29:52 +0430

    examples: update to call the helper
    Missed this in the #1 pull request sorry 😅.

--- example/test.js
+++ example/test.js
@@ -2,7 +2,7 @@ const BackTracker = require("../dist/index.js").BackTracker;
 
 function epic() {
 	console.log("Okay. This is epic.");
-	console.log(BackTracker.stack.first);
+	console.log(BackTracker.stack.first());
 }
 
 module.exports = epic;
