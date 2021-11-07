4a98167d711d2db5ca85de0273669926b024e228
Papa <bradleyjplatt@yahoo.com>
2021-11-06 17:44:07 -0700

    Remove dist from git

--- dist/Stack.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Stack extends Array<import("./Caller")> {
-    constructor();
-    first(): import("./Caller");
-    first(amount: number): Array<import("./Caller")>;
-    last(): import("./Caller");
-    last(amount: number): Array<import("./Caller")>;
-}
-export = Stack;
