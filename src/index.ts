bfba3553c8a89c5ef56379bf5ac083896d449045
Papa <bradleyjplatt@yahoo.com>
2022-07-10 11:42:14 -0700

    Add support for file URLs in error stacks

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import Caller from "./Caller";
 import Stack from "./Stack";
 
-const [evalreg, namedreg, anonymousreg, notnamedreg, somethingreg] = [
+const [evalreg, namedreg, anonymousreg, notnamedreg, somethingreg, filereg] = [
 	/at eval \(eval at ([\w_.<>]*) \(([^*?"<>]*):(\d+):(\d+)\),? ?(?:[^:]+:\d+:\d+)?\)/, // expression for global.eval
 	/at (async)? ?([\w_.<>]+)? \(([^*?"<>]*):(\d+):(\d+)\)/, // expression for named scopes
 	/at (.[\w_.<>]*)? ?\((<anonymous>)\)/, // expression for anonymous scopes
 	/at ([^*?"<>]*):(\d+):(\d+)/, // expression for scopes not named
-	/((?:.[\w_.<>]*)? ?(?:.[\w_.<>]*)? ?\[(?:as)? ?(?:.[\w_.<>]*)\])? ?\(([^*?"<>]*):(\d+):(\d+)\)/ // expression for something??? I'm not sure what to call it tbh.
+	/((?:.[\w_.<>]*)? ?(?:.[\w_.<>]*)? ?\[(?:as)? ?(?:.[\w_.<>]*)\])? ?\(([^*?"<>]*):(\d+):(\d+)\)/, // expression for something??? I'm not sure what to call it tbh.
+	/at (async)? file:\/{2,3}([^:]):(\d+):(\d+)/ // expression for file urls
 ];
 
 class BackTracker {
@@ -20,7 +21,7 @@ class BackTracker {
 		const stack = new Error().stack;
 		const callers = new Stack();
 		if (!stack) {
-			callers.push(new Caller({ path: __dirname, async: false, scope: "root", line: 0, column: 0, anonymous: true }));
+			callers.push(new Caller({ path: __dirname || "backtracker:internal/index", async: false, scope: "root", line: 0, column: 0, anonymous: true }));
 			return callers;
 		}
 
@@ -31,7 +32,7 @@ class BackTracker {
 			const test1 = evalreg.exec(frame);
 
 			if (test1 && test1.length > 0) {
-				callers.push(new Caller({ path: test1[2], async: false, scope: test1[1], line: Number(test1[3]), column: Number(test1[4]), anonymous: test1[5] ? true : false }));
+				callers.push(new Caller({ path: test1[2], async: false, scope: test1[1], line: Number(test1[3]), column: Number(test1[4]), anonymous: !!test1[5] }));
 				continue;
 			}
 
@@ -43,7 +44,7 @@ class BackTracker {
 
 			const test3 = namedreg.exec(frame);
 			if (test3 && test3.length > 0) {
-				callers.push(new Caller({ path: test3[3], async: test3[1] ? true : false, scope: test3[2], line: Number(test3[4]), column: Number(test3[5]), anonymous: false }));
+				callers.push(new Caller({ path: test3[3], async: !!test3[1], scope: test3[2], line: Number(test3[4]), column: Number(test3[5]), anonymous: false }));
 				continue;
 			}
 
@@ -56,6 +57,13 @@ class BackTracker {
 			const test5 = notnamedreg.exec(frame);
 			if (test5 && test5.length > 0) {
 				callers.push(new Caller({ path: test5[1], async: false, scope: "root", line: Number(test5[2]), column: Number(test5[3]), anonymous: false }));
+				continue;
+			}
+
+			const test6 = filereg.exec(frame);
+			if (test6 && test6.length > 0) {
+				callers.push(new Caller({ path: test6[2], async: !!test6[1], scope: "file", line: Number(test6[3]), column: Number(test6[4]), anonymous: false }));
+				continue;
 			}
 		}
 
