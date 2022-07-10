d3158d46c203e82b6db7a0ead2e2efaa58131f1f
Papa <bradleyjplatt@yahoo.com>
2022-07-10 12:05:03 -0700

    Fix file url regex for Windows

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const [evalreg, namedreg, anonymousreg, notnamedreg, somethingreg, filereg] = [
 	/at (.[\w_.<>]*)? ?\((<anonymous>)\)/, // expression for anonymous scopes
 	/at ([^*?"<>]*):(\d+):(\d+)/, // expression for scopes not named
 	/((?:.[\w_.<>]*)? ?(?:.[\w_.<>]*)? ?\[(?:as)? ?(?:.[\w_.<>]*)\])? ?\(([^*?"<>]*):(\d+):(\d+)\)/, // expression for something??? I'm not sure what to call it tbh.
-	/at (async)? file:\/{2,3}([^:]):(\d+):(\d+)/ // expression for file urls
+	/at (async)? ?file:\/\/\/([\w\d:\/\. \(\)_\-,]+):(\d+):(\d+)/ // expression for file urls
 ];
 
 class BackTracker {
