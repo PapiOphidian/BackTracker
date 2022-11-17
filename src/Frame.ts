607c843e342c2e0c73fde7946d141f9969416be0
Papa <bradleyjplatt@yahoo.com>
2022-11-17 13:03:41 -0700

    Adds extra details to Frames for source maps
    - Uses a regex to parse Error stacks formatted by the runtime to extract only filename, line and column from source maps if available, otherwise, falls back to what the CallSite provides (CallSite does not provide source map support)

--- src/Frame.ts
+++ src/Frame.ts
@@ -4,6 +4,11 @@ class Frame {
 	public path: string;
 	public dir: string;
 	public filename: string;
+	public srcPath: string;
+	public srcDir: string;
+	public srcFilename: string;
+	public srcLine: number;
+	public srcColumn: number;
 	public async: boolean;
 	public scope: string;
 	public line: number;
@@ -11,10 +16,15 @@ class Frame {
 	public anonymous: boolean;
 	public parent: Frame | null;
 
-	public constructor(details: { path: string; async: boolean; scope: string; line: number; column: number; anonymous: boolean; }) {
+	public constructor(details: { path: string; async: boolean; scope: string; line: number; column: number; anonymous: boolean; srcPath: string; srcLine: number; srcColumn: number; }) {
 		this.path = details.path;
 		this.dir = path.dirname(details.path);
 		this.filename = path.basename(details.path);
+		this.srcPath = details.srcPath
+		this.srcDir = path.dirname(details.srcPath);
+		this.srcFilename = path.basename(details.srcPath);
+		this.srcLine = details.srcLine;
+		this.srcColumn = details.srcColumn;
 		this.async = details.async;
 		this.scope = details.scope;
 		this.line = details.line;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Frame from "./Frame";
 import Stack from "./Stack";
 
+const filenameRegex = /(?:[^( ]+)? ?\(?(?:file\:\/\/\/)?([^*"\(\)]+):(\d+):(\d+)\)?/;
+
 class BackTracker {
 	public parent: Frame;
 
@@ -15,7 +17,11 @@ class BackTracker {
 		Error.prepareStackTrace = _;
 		const es: Array<NodeJS.CallSite> = stack as unknown as Array<NodeJS.CallSite> || [];
 
-		const s = new Stack()
+		const s = new Stack();
+		const runtimeFormatted = new Error().stack || "";
+		const split = runtimeFormatted.split("\n");
+		const firstIndex = split.findIndex(f => f === "Error");
+		const stringFrames = split.slice(firstIndex + 1 || 1).map(item => item.trim());
 
 		let index = 0;
 		for (const c of es) {
@@ -25,17 +31,27 @@ class BackTracker {
 			}
 			const scope = (c as unknown as { isPromiseAll(): boolean }).isPromiseAll()
 			? "Promise.all"
-			: c.getFunctionName() || null
+			: c.getFunctionName() || null;
 
 			const filename = c.getFileName();
 
+			const match = stringFrames[index]?.match(filenameRegex);
+
+			const pathToUse = c.getEvalOrigin() || filename || "<anonymous>";
+			const lineToUse = c.getLineNumber() || 0;
+			const columnToUse = c.getColumnNumber() || 0;
+
 			const frame = new Frame({
-				path: c.getEvalOrigin() || filename || "<anonymous>",
+				path: pathToUse,
 				async: (c as unknown as { isAsync(): boolean }).isAsync(),
 				scope: scope || "<anonymous>",
-				line: c.getLineNumber() || 0,
-				column: c.getColumnNumber() || 0,
-				anonymous: !filename || !scope
+				line: lineToUse,
+				column: columnToUse,
+				anonymous: !filename || !scope,
+
+				srcPath: match?.[1] || pathToUse,
+				srcLine: Number(match?.[2] || lineToUse),
+				srcColumn: Number(match?.[3] || columnToUse)
 			});
 
 			s.push(frame);
